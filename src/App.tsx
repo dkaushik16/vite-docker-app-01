0efd7897e9871a8a635e06fbe9395603343bb45c
divyanshu1625 <dkaushikd99@gmail.com>
2026-05-28 23:15:04 +0530

    pipeline automation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
-    <div className="text-3xl bg-slate-700 p-8 text-white h-screen">
-      Vite + React dockerized application on azure
+    <div className="text-3xl bg-slate-700 p-8 flex flex-col items-center justify-center gap-6 text-white h-screen">
+      <p>Vite + React dockerized application on azure</p>
+      <p>deployed on container and app service</p>
     </div>
-  )
+  );
 }
