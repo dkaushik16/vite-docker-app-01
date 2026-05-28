c80b891c1a77b504a7542341d9605ebc3879fbca
divyanshu1625 <dkaushikd99@gmail.com>
2026-05-28 13:47:56 +0530

    the first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <div className="text-3xl bg-black text-white h-full">App</div>
+    <div className="text-3xl bg-slate-700 p-8 text-white h-screen">
+      Vite + React dockerized application
+    </div>
   )
 }
